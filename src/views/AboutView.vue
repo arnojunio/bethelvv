59952a39fe1f5a61f1969ba534e38cb54ae03e3f
Arno Morais <arno.morais@ufv.br>
2023-04-04 20:11:31 -0300

    salvando alteracoes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div class="row event-cover">
+
+    </div>
 </template>
+<script>
+export default {
+  name: 'AboutView'
+}
+</script>
+<style>
+.event-cover {
+  background-image: url('../assets/face2.png');
+  transform: translate3d(0px, 0px, 0px);
+    min-height: 40vh!important;
+    background-color: #b2afab;
+    background-position: 50%;
+    background-size: cover;
+    min-height: 130vh;
+    max-height: 999px;
+    overflow: hidden;
+    position: relative;
+    /* width: 100%; */
+    z-index: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
